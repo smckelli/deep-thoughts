c77486f56443650f44cf835b26bc982a1161b187
smckelli <smckelli@gmail.com>
2022-07-17 17:42:55 -0500

    forms built

--- client/src/App.js
+++ client/src/App.js
@@ -57,13 +57,13 @@ function App() {
                 path="/signup" 
                 element={<Signup />} 
               />
-              <Route path="/profile">
-                <Route path=":username" element={<Profile />} />
-                <Route path="" element={<Profile />} />
-              </Route>
-              <Route
-                path="/thought/:id"
-                element={<SingleThought />}
+              <Route 
+                path="/profile" 
+                element={<Profile />} 
+              />
+              <Route 
+                path="/thought/:id" 
+                element={<SingleThought />} 
               />
               <Route 
                 path="*" 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const Login = (props) => {
@@ -17,20 +18,26 @@ const Login = (props) => {
     });
   };
 
-// submit form
-const handleFormSubmit = async event => {
-  event.preventDefault();
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-  try {
-    const { data } = await login({
-      variables: { ...formState }
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
     });
-  
-    Auth.login(data.login.token);
-  } catch (e) {
-    console.error(e);
-  }
-};
+  };
 
   return (
     <main className="flex-row justify-center mb-4">
@@ -61,6 +68,7 @@ const handleFormSubmit = async event => {
                 Submit
               </button>
             </form>
+
             {error && <div>Login failed</div>}
           </div>
         </div>
