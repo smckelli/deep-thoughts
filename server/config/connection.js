80b8883bae6897169066789bdca437686ccc9556
smckelli <smckelli@gmail.com>
2022-07-17 19:51:01 -0500

    yet another

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/deep-thoughts";
-
 mongoose.connect(
-MONGODB_URI,
+  process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/deep-thoughts',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
